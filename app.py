922f60001a87538c9575bef9c8b7d318ef439c00
Justin Lyons <lyonjust@outlook.com>
2022-06-10 21:18:55 +1000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+N_SIMS = 1000
+SEED = 0
+
+rng = np.random.default_rng(SEED)
+
 n_shots_home = st.sidebar.number_input("Number of shots (home team)", 0, 30, 1)
 rows_home = st.columns(n_shots_home)
 
@@ -20,4 +25,28 @@ for i, x in enumerate(rows_away):
     shot_xg = x.number_input(f"xG of away shot # {i+1}",min_value=0.01, max_value=0.99, step=0.01, key=i)
     away_shots_xg.append(shot_xg)
 
-print(home_shots_xg)
+# print(home_shots_xg)
+
+home_goals = []
+away_goals = []
+
+for i in range(N_SIMS):
+    for shot_xg in home_shots_xg:
+        random = rng.random()
+        if random > shot_xg:
+            outcome = 1
+        else:
+            outcome = 0
+        home_goals.append(outcome)
+
+for i in range(N_SIMS):
+    for shot_xg in away_shots_xg:
+        random = rng.random()
+        if random > shot_xg:
+            outcome = 1
+        else:
+            outcome = 0
+        away_goals.append(outcome)
+
+st.text('home goals: ' + str(home_goals))
+
