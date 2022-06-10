ea7c3e1afe79662d97b6694118451a0160592413
Justin Lyons <lyonjust@outlook.com>
2022-06-10 21:01:40 +1000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ n_shots_home = st.sidebar.number_input("Number of shots (away team)", 0, 30, 1)
 rows_away = st.columns(n_shots_home)
 
 for i, x in enumerate(rows_home):
-    x.number_input(f"xG of home shot # {i}",min_value=0, max_value=1, step=0.01, key=i)
+    x.number_input(f"xG of home shot # {i}",min_value=0.01, max_value=0.99, step=0.01, key=i)
 
 for i, x in enumerate(rows_away):
-    x.number_input(f"xG of away shot # {i}",min_value=0, max_value=1, step=0.01, key=i)
+    x.number_input(f"xG of away shot # {i}",min_value=0.01, max_value=0.99, step=0.01, key=i)
