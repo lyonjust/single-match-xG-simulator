c03e7e37f50aa7fe5eec06af1c3c502916db11af
Justin Lyons <lyonjust@outlook.com>
2022-06-11 22:21:53 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 N_SIMS = 10000
 SEED = 0
 
 rng = np.random.default_rng(SEED)
 
-home_shots = st.text_input('Please enter the xG of all home team shots, separated by a comma (",")')
-away_shots = st.text_input('Please enter the xG of all away team shots, separated by a comma (",")')
+home_shots = st.text_input(
+    'Please enter the xG of all home team shots, separated by a comma (",")')
+away_shots = st.text_input(
+    'Please enter the xG of all away team shots, separated by a comma (",")')
+
 
 def xg_to_array(xg_string):
     return [float(x.strip()) for x in xg_string.split(',')]
 
+
 home_xg = xg_to_array(home_shots)
 away_xg = xg_to_array(away_shots)
+
 
 def simulate_chances(rng, number_of_sims, xg_of_chances):
     '''
@@ -35,13 +42,15 @@ def simulate_chances(rng, number_of_sims, xg_of_chances):
 
     return goals_scored
 
+
 home_goals = simulate_chances(rng, N_SIMS, home_xg)
 away_goals = simulate_chances(rng, N_SIMS, away_xg)
 home_margin = home_goals - away_goals
 
 match_outcomes = np.vstack((home_goals, away_goals, home_margin))
 
-df_match_outcomes = pd.DataFrame(match_outcomes.T, columns=['home_goals', 'away_goals', 'home_margin'])
+df_match_outcomes = pd.DataFrame(match_outcomes.T, columns=[
+                                 'home_goals', 'away_goals', 'home_margin'])
 
 conditions = [
     df_match_outcomes['home_margin'] > 0,
@@ -53,9 +62,19 @@ choices = [
     'Away win'
 ]
 
-df_match_outcomes['match_outcome'] = np.select(condlist=conditions, choicelist=choices, default='Draw')
+df_match_outcomes['match_outcome'] = np.select(
+    condlist=conditions, choicelist=choices, default='Draw')
 
-df_grouped = df_match_outcomes[['match_outcome', 'home_goals']].groupby('match_outcome').count()
+df_grouped = df_match_outcomes[['match_outcome',
+                                'home_goals']].groupby('match_outcome').count()
 df_grouped.columns = ['count']
+df_grouped = df_grouped.reindex(['Home win', 'Draw', 'Away win'])
 
 st.write(df_grouped)
+
+fig, ax = plt.subplots()
+
+sns.histplot(data=df_match_outcomes, x='home_margin', discrete=True,
+             stat='density', hue='match_outcome', ax=ax, zorder=1)
+
+st.pyplot(fig=fig)
