92a85fed261d0b04476591688d7e024c660226b5
Justin Lyons <lyonjust@outlook.com>
2022-07-08 16:29:38 +1000

    redirect on old url

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="xG simulator",
+    page_icon="⚽",
+    #    layout="wide",
+    #    initial_sidebar_state="expanded"
+)
+
+st.title('Single match xG simulator')
+
+st.write(
+    "This app has been updated, please [click here](https://lyonjust-single-match-xg-simulator-custom-match-44h8y9.streamlitapp.com)")
