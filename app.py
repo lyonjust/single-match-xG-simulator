325f1d937e62da5ee427dac89ed0ee4a9510d91b
Justin Lyons <lyonjust@outlook.com>
2022-11-01 12:41:32 +1100

    Include FotMob

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import matplotlib as mpl
 import aiohttp
 import asyncio
 from understat import Understat
+import requests
 
 from functions import simulate
 
@@ -40,13 +41,13 @@ caption = caption + '\nUnderstat match IDs are found from the last part of the m
 
 st.caption(caption)
 
-custom_or_understat = st.radio(
+custom_or_understat _or_fotmob = st.radio(
     "Simulate a...",
-    ('Custom match', 'Understat match ID'))
+    ('Custom match', 'Understat match ID', 'FotMob match ID'))
 
 st.header('Input')
 
-if custom_or_understat == 'Custom match':
+if custom_or_understat _or_fotmob == 'Custom match':
 
     st.caption("Please enter the xG of each team's shots in the input boxes below.\n\nIndividual xG values should be separated by a comma (',')")
 
@@ -91,7 +92,7 @@ if custom_or_understat == 'Custom match':
 
     st.pyplot(fig=fig)
 
-else:
+elif custom_or_understat _or_fotmob == 'Understat match ID':
     st.caption("Please enter the match ID of an Understat match, e.g. 16669")
 
     understat_match_id = st.text_input(
@@ -158,6 +159,85 @@ else:
 
         fig, ax = simulate.plot_margins(df_match_outcomes, int(home_goals_actual), int(away_goals_actual), simulated_home_win_percent,
                                         simulated_draw_percent, simulated_away_win_percent, percentage_of_sims_matching_actual_score, total_home_xg, total_away_xg, match_date=match_date, home_team=home_team, away_team=away_team)
+
+        st.pyplot(fig=fig)
+
+        fig, ax = simulate.plot_exact_scores(df_match_outcomes)
+
+        st.pyplot(fig=fig)
+
+else:  # fotmob
+    st.caption("Please enter the match ID of an FotMob match, e.g. 3978853")
+
+    fotmob_match_id = st.text_input(
+        'FotMob match ID')
+
+    # cater for matches/teams with no shots?
+
+    url_base = 'https://www.fotmob.com/api/'
+    url_match_details = 'matchDetails?matchId='
+
+    if fotmob_match_id:
+        url_complete = url_base + url_match_details + fotmob_match_id
+
+        match_summary = requests.get(url_complete).json()
+        shot_summary = match_summary['content']['shotmap']['shots']
+
+        match_date = pd.to_datetime(
+            match_summary['general']['matchTimeUTCDate'])
+
+        home_team_name = match_summary['general']['homeTeam']['name']
+        home_team_id = match_summary['general']['homeTeam']['id']
+
+        away_team_name = match_summary['general']['awayTeam']['name']
+        away_team_id = match_summary['general']['awayTeam']['id']
+
+        home_goals_actual = [team['score'] for team in match_summary['header']
+                             ['teams'] if team['id'] == home_team_id][0]
+        away_goals_actual = [team['score'] for team in match_summary['header']
+                             ['teams'] if team['id'] == away_team_id][0]
+
+        home_shots_list = [[shot['min'], shot['playerName'], shot['expectedGoals'],
+                            shot['eventType']] for shot in shot_summary if shot['teamId'] == home_team_id]
+        away_shots_list = [[shot['min'], shot['playerName'], shot['expectedGoals'],
+                            shot['eventType']] for shot in shot_summary if shot['teamId'] == away_team_id]
+
+        columns = ['Minute', 'Player', 'xG', 'Outcome']
+
+        df_home_shots = pd.DataFrame(home_shots_list, columns=columns)
+        df_away_shots = pd.DataFrame(home_shots_list, columns=columns)
+
+        total_home_xg = df_home_shots['xG'].sum()
+        total_away_xg = df_away_shots['xG'].sum()
+
+        columns = ['Minute', 'Player', 'xG', 'Outcome']
+
+        home_goals = simulate.simulate_chances(
+            rng, N_SIMS, df_home_shots['xG'])
+        away_goals = simulate.simulate_chances(
+            rng, N_SIMS, df_away_shots['xG'])
+        home_margin = home_goals - away_goals
+
+        df_match_outcomes = simulate.get_match_outcomes(
+            home_goals, away_goals, home_margin)
+
+        simulated_home_win_percent, simulated_away_win_percent, simulated_draw_percent, percentage_of_sims_matching_actual_score = simulate.get_sims_matching_score(
+            df_match_outcomes, int(home_goals_actual), int(away_goals_actual))
+
+        st.header('Shot details')
+
+        st.subheader(home_team_name + ' (home)')
+
+        st.dataframe(df_home_shots)
+
+        st.subheader(away_team_name + ' (away)')
+
+        st.dataframe(df_away_shots)
+
+        st.header('Match outcomes')
+
+        fig, ax = simulate.plot_margins(df_match_outcomes, int(home_goals_actual), int(away_goals_actual), simulated_home_win_percent,
+                                        simulated_draw_percent, simulated_away_win_percent, percentage_of_sims_matching_actual_score, total_home_xg, total_away_xg, match_date=match_date, home_team=home_team_name, away_team=away_team_name)
 
         st.pyplot(fig=fig)
 
