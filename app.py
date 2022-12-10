952415bd6bc3701d6e9259280705e2385963a782
Justin Lyons <lyonjust@outlook.com>
2022-12-10 22:49:54 +1100

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ caption = caption + 'It then provides a summary of the possible match outcomes.\
 caption = caption + \
     'The purpose is to reinforce understanding and improve language around xG, '
 caption = caption + 'as well as highlight the limitations of drawing absolute conclusions from just the overall aggregate single match xG comparison.\n\n'
-caption = caption + 'If you choose a "custom match", you can enter any sequence of custom xG values for a hypothetical home and away team, as well as a hypothetical match result.\n\n'
+caption = caption + 'If you choose a **custom match**, you can enter any sequence of custom xG values for a hypothetical home and away team, as well as a hypothetical match result.\n\n'
 caption = caption + \
-    'If you choose an "Understat match ID" or "FotMob match ID", you can enter a valid match ID from Understat (e.g. "16669") or FotMob (e.g. "3854572") and the app will perform the simulation based on the shot details recorded for this match. Penalty shootouts are excluded from these analyses.\n'
+    'If you choose an **Understat match ID** or **FotMob match ID**, you can enter a valid match ID from Understat (e.g. "16669") or FotMob (e.g. "3854572") and the app will perform the simulation based on the shot details recorded for this match. Penalty shootouts are excluded from these analyses.\n'
 
 
 understat_helper = '\nUnderstat match IDs are found from the last part of the match details URL: understat.com/match/<match_id>\n'
